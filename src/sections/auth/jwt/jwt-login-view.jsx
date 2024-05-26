c778698ab8d3710a2da453aaab2211a912493db9
webwebweb5 <phiriyakornman@gmail.com>
2024-05-26 16:36:21 +0700

    simple login design

--- src/sections/auth/jwt/jwt-login-view.jsx
+++ src/sections/auth/jwt/jwt-login-view.jsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from 'next/navigation';
 
+import { Box } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import LoadingButton from '@mui/lab/LoadingButton';
@@ -20,8 +21,26 @@ export default function JwtLoginView() {
 
   return (
     <>
-      <Stack spacing={2} sx={{ mb: 5 }}>
-        <Typography variant="h4">Sign in to Questify</Typography>
+      <Stack spacing={1} sx={{ mb: 5, mx: 6 }} alignItems="center">
+        <Box sx={{ position: 'relative' }}>
+          <Typography variant="h4">Sign in to Questify</Typography>
+          <Box
+            sx={{
+              right: 0,
+              bottom: 4,
+              width: 70,
+              height: 10,
+              opacity: 0.48,
+              bgcolor: 'primary.main',
+              position: 'absolute',
+              zIndex: -1,
+            }}
+          />
+        </Box>
+        <Typography variant="subtitle1" sx={{ color: 'text.secondary' }} textAlign="center">
+          Revolutionizing software engineering education with AI, cloud computing, and modern web
+          technologies!
+        </Typography>
       </Stack>
 
       <Stack spacing={2.5}>
@@ -32,6 +51,16 @@ export default function JwtLoginView() {
           onClick={handleLogin}
           variant="contained"
         >
+          <Box
+            component="div"
+            sx={{
+              height: 24,
+              display: 'inline-flex',
+              mr: 2,
+            }}
+          >
+            <Box component="img" alt="cmu-logo" src="/assets/images/sub-logo/cmu-sub-logo.png" />
+          </Box>
           Login with CMU Account
         </LoadingButton>
       </Stack>
