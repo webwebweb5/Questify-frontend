ed3c180078df9b87ba8c0fcc8cac6a45043bf9d7
webwebweb5 <phiriyakornman@gmail.com>
2024-06-21 23:14:39 +0700

    member list view

--- src/sections/classroom/member-list.jsx
+++ src/sections/classroom/member-list.jsx
@@ -1,29 +1,192 @@
-import { m } from 'framer-motion';
+import * as Yup from 'yup';
+import { useMemo } from 'react';
 import PropTypes from 'prop-types';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 
-import { Box, Stack } from '@mui/material';
+import {
+  Box,
+  Stack,
+  Table,
+  Avatar,
+  TableRow,
+  MenuItem,
+  TableBody,
+  TableCell,
+  TableHead,
+  IconButton,
+  Typography,
+  TableContainer,
+} from '@mui/material';
 
-import { varFade, MotionContainer } from 'src/components/animate';
+import { useResponsive } from 'src/hooks/use-responsive';
+import { useCurrentRole } from 'src/hooks/use-current-role';
+
+import Iconify from 'src/components/iconify';
+import Scrollbar from 'src/components/scrollbar';
+import FormProvider, { RHFSelect } from 'src/components/hook-form';
+import CustomPopover, { usePopover } from 'src/components/custom-popover';
 
 // ----------------------------------------------------------------------
 
-export default function MemberList({ users }) {
-  if (users.length === 0) {
+const TABLE_HEAD = [
+  { id: 'name', label: 'Name', minWidth: 180, width: { xs: 300, lg: 450 } },
+  { id: 'mail', label: 'Email', align: 'left' },
+  { id: 'actions', label: '', align: 'right' },
+];
+
+// ----------------------------------------------------------------------
+
+export default function MemberList({ users, assignLab = false }) {
+  const mdUp = useResponsive('up', 'md');
+
+  const role = useCurrentRole();
+
+  const popover = usePopover();
+
+  const LabVersionSchema = Yup.object().shape({
+    labVersion: Yup.string()
+      .required('language is required')
+      .oneOf(['Java', 'JavaScript', 'Python', 'C'], 'Invalid language'),
+  });
+
+  const defaultValues = useMemo(
+    () => ({
+      labVersion: '',
+    }),
+    []
+  );
+
+  const methods = useForm({
+    resolver: yupResolver(LabVersionSchema),
+    defaultValues,
+  });
+
+  if (users?.length === 0) {
     return <Box>No User...</Box>;
   }
+
   return (
-    <MotionContainer>
-      <Stack spacing={3} sx={{ mt: 5 }}>
-        {users.map((user) => (
-          <Box key={user.userId} component={m.div} variants={varFade().inUp}>
-            User List Here
-          </Box>
-        ))}
-      </Stack>
-    </MotionContainer>
+    <>
+      <TableContainer sx={{ overflow: 'unset' }}>
+        <Scrollbar>
+          <Table sx={{ minWidth: 650 }}>
+            <TableHead>
+              <TableRow>
+                {TABLE_HEAD.map((headCell) => (
+                  <TableCell
+                    key={headCell.id}
+                    align={headCell.align || 'left'}
+                    sx={{
+                      backgroundColor: 'transparent',
+                      minWidth: headCell.minWidth,
+                      // eslint-disable-next-line no-nested-ternary
+                      width: headCell.width
+                        ? mdUp
+                          ? headCell.width.lg
+                          : headCell.width.xs
+                        : 'auto',
+                    }}
+                  >
+                    {headCell.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+
+            <TableBody>
+              {users?.map((user) => (
+                <TableRow>
+                  <TableCell sx={{ borderTopRightRadius: 0, borderBottomRightRadius: 0 }}>
+                    <Stack direction="row" alignItems="center" spacing={2}>
+                      <Avatar
+                        sx={{
+                          width: 36,
+                          height: 36,
+                        }}
+                      >
+                        {user.firstName_EN.charAt(0)}
+                      </Avatar>
+                      <Box sx={{ display: 'flex', flexDirection: 'column', width: 'fit-content' }}>
+                        <Typography sx={{ width: 'fit-content' }}>
+                          {user.displayName
+                            .toLowerCase()
+                            .split(' ')
+                            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                            .join(' ')}
+                        </Typography>
+                        <Typography
+                          component="span"
+                          variant="caption"
+                          color="text.disabled"
+                          sx={{ mt: 0.5, width: 'fit-content' }}
+                        >
+                          {user.studentId}
+                        </Typography>
+                      </Box>
+                    </Stack>
+                  </TableCell>
+                  <TableCell align="left" sx={{ borderRadius: 0 }}>
+                    <Typography>{user.email}</Typography>
+                  </TableCell>
+                  {role === 'ProfAcc' &&
+                    (assignLab ? (
+                      <TableCell>
+                        <FormProvider methods={methods}>
+                          <Stack sx={{ width: 'fit-content', minWidth: 140 }}>
+                            <RHFSelect name="labVersion" label="Lab Version">
+                              <MenuItem value="v1">Version 1</MenuItem>
+                              <MenuItem value="v2">Version 2</MenuItem>
+                              <MenuItem value="v3">Version 3</MenuItem>
+                            </RHFSelect>
+                          </Stack>
+                        </FormProvider>
+                      </TableCell>
+                    ) : (
+                      <TableCell align="right">
+                        <IconButton onClick={popover.onOpen}>
+                          <Iconify icon="eva:more-vertical-fill" />
+                        </IconButton>
+                      </TableCell>
+                    ))}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Scrollbar>
+      </TableContainer>
+
+      <CustomPopover
+        open={popover.open}
+        onClose={popover.onClose}
+        arrow="right-top"
+        sx={{ width: 140 }}
+      >
+        <MenuItem
+          onClick={() => {
+            popover.onClose();
+          }}
+          sx={{ color: 'primary.main' }}
+        >
+          <Iconify icon="carbon:chart-histogram" />
+          Grades
+        </MenuItem>
+
+        <MenuItem
+          onClick={() => {
+            popover.onClose();
+          }}
+          sx={{ color: 'error.main' }}
+        >
+          <Iconify icon="solar:trash-bin-trash-bold" />
+          Remove
+        </MenuItem>
+      </CustomPopover>
+    </>
   );
 }
 
 MemberList.propTypes = {
   users: PropTypes.array,
+  assignLab: PropTypes.bool,
 };
--- src/sections/classroom/view/member-list-view.jsx
+++ src/sections/classroom/view/member-list-view.jsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { useSnackbar } from 'notistack';
+import { useParams } from 'next/navigation';
+
 import {
   Box,
   Stack,
@@ -20,11 +23,17 @@ import {
 
 import { useResponsive } from 'src/hooks/use-responsive';
 import { useCurrentRole } from 'src/hooks/use-current-role';
+import { useCopyToClipboard } from 'src/hooks/use-copy-to-clipboard';
 
+import { useGetClassroomById } from 'src/api/classroom';
+
+import Label from 'src/components/label';
 import Iconify from 'src/components/iconify';
 import Scrollbar from 'src/components/scrollbar';
 import { useSettingsContext } from 'src/components/settings';
 import CustomPopover, { usePopover } from 'src/components/custom-popover';
+
+import MemberList from '../member-list';
 
 // ----------------------------------------------------------------------
 
@@ -43,15 +52,37 @@ export default function MemberListView() {
 
   const popover = usePopover();
 
+  const params = useParams();
+
+  const { classroom } = useGetClassroomById(params.cid);
+
+  const { copy } = useCopyToClipboard();
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const onCopy = (invitationCode) => {
+    if (invitationCode) {
+      enqueueSnackbar(`Copied: ${invitationCode}`);
+      copy(invitationCode);
+    }
+  };
+
   return (
     <Container maxWidth={settings.themeStretch ? false : 'xl'}>
       <Stack spacing={2} sx={{ mb: 6 }}>
         <Stack direction="row" justifyContent="space-between">
-          <Typography variant="h4">Professors</Typography>
+          <Stack direction="row" alignItems="center" justifyContent="center" spacing={1}>
+            <Typography variant="h4">Professors</Typography>
+            <Label>1</Label>
+          </Stack>
 
           <Tooltip title="Invitation Code" placement="top" arrow>
-            <Button startIcon={<Iconify icon="carbon:copy" />} sx={{ px: 1.5 }}>
-              748e2408
+            <Button
+              startIcon={<Iconify icon="carbon:copy" />}
+              sx={{ px: 1.5 }}
+              onClick={() => onCopy(classroom?.invitationCode)}
+            >
+              {classroom?.invitationCode}
             </Button>
           </Tooltip>
         </Stack>
@@ -92,30 +123,36 @@ export default function MemberListView() {
                           height: 36,
                         }}
                       >
-                        P
+                        {classroom?.professor?.firstName_EN?.charAt(0)}
                       </Avatar>
                       <Box sx={{ display: 'flex', flexDirection: 'column', width: 'fit-content' }}>
-                        <Typography sx={{ width: 'fit-content' }}>Pathathai Nalumpoon</Typography>
+                        <Typography sx={{ width: 'fit-content' }}>
+                          {classroom?.professor?.displayName
+                            .toLowerCase()
+                            .split(' ')
+                            .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                            .join(' ')}
+                        </Typography>
                         <Typography
                           component="span"
                           variant="caption"
                           color="text.disabled"
                           sx={{ mt: 0.5, width: 'fit-content' }}
                         >
-                          542118024
+                          {classroom?.professor?.professorId}
                         </Typography>
                       </Box>
                     </Stack>
                   </TableCell>
                   <TableCell align="left" sx={{ borderRadius: 0 }}>
-                    <Typography>pathathai.n@cmu.ac.th</Typography>
+                    <Typography>{classroom?.professor?.email}</Typography>
                   </TableCell>
                   {role === 'ProfAcc' && (
                     <TableCell
                       align="right"
                       sx={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
                     >
-                      <IconButton>
+                      <IconButton onClick={popover.onOpen}>
                         <Iconify icon="eva:more-vertical-fill" />
                       </IconButton>
                     </TableCell>
@@ -137,139 +174,32 @@ export default function MemberListView() {
           onClick={() => {
             popover.onClose();
           }}
-          sx={{ color: 'error.main' }}
+          sx={{ color: 'primary.main' }}
         >
-          <Iconify icon="solar:trash-bin-trash-bold" />
-          Remove
+          <Iconify icon="carbon:chart-histogram" />
+          Grades
         </MenuItem>
 
         <MenuItem
           onClick={() => {
             popover.onClose();
           }}
-          sx={{ color: 'primary.main' }}
+          sx={{ color: 'error.main' }}
         >
-          <Iconify icon="carbon:chart-histogram" />
-          Grades
+          <Iconify icon="solar:trash-bin-trash-bold" />
+          Remove
         </MenuItem>
       </CustomPopover>
 
       <Stack spacing={2}>
         <Stack direction="row" justifyContent="space-between">
-          <Typography variant="h4">Students</Typography>
+          <Stack direction="row" alignItems="center" justifyContent="center" spacing={1}>
+            <Typography variant="h4">Students</Typography>
+            <Label>{classroom?.students?.length}</Label>
+          </Stack>
         </Stack>
 
-        <TableContainer sx={{ overflow: 'unset' }}>
-          <Scrollbar>
-            <Table sx={{ minWidth: 650 }}>
-              <TableHead>
-                <TableRow>
-                  {TABLE_HEAD.map((headCell) => (
-                    <TableCell
-                      key={headCell.id}
-                      align={headCell.align || 'left'}
-                      sx={{
-                        backgroundColor: 'transparent',
-                        minWidth: headCell.minWidth,
-                        // eslint-disable-next-line no-nested-ternary
-                        width: headCell.width
-                          ? mdUp
-                            ? headCell.width.lg
-                            : headCell.width.xs
-                          : 'auto',
-                      }}
-                    >
-                      {headCell.label}
-                    </TableCell>
-                  ))}
-                </TableRow>
-              </TableHead>
-
-              <TableBody>
-                <TableRow>
-                  <TableCell sx={{ borderTopRightRadius: 0, borderBottomRightRadius: 0 }}>
-                    <Stack direction="row" alignItems="center" spacing={2}>
-                      <Avatar
-                        sx={{
-                          width: 36,
-                          height: 36,
-                        }}
-                      >
-                        P
-                      </Avatar>
-                      <Box sx={{ display: 'flex', flexDirection: 'column', width: 'fit-content' }}>
-                        <Typography sx={{ width: 'fit-content' }}>
-                          Phiriyakorn Maneekongrit
-                        </Typography>
-                        <Typography
-                          component="span"
-                          variant="caption"
-                          color="text.disabled"
-                          sx={{ mt: 0.5, width: 'fit-content' }}
-                        >
-                          642115031
-                        </Typography>
-                      </Box>
-                    </Stack>
-                  </TableCell>
-                  <TableCell align="left" sx={{ borderRadius: 0 }}>
-                    <Typography>phiriyakorn_m@cmu.ac.th</Typography>
-                  </TableCell>
-                  {role === 'ProfAcc' && (
-                    <TableCell
-                      align="right"
-                      sx={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
-                    >
-                      <IconButton>
-                        <Iconify icon="eva:more-vertical-fill" />
-                      </IconButton>
-                    </TableCell>
-                  )}
-                </TableRow>
-                <TableRow>
-                  <TableCell sx={{ borderTopRightRadius: 0, borderBottomRightRadius: 0 }}>
-                    <Stack direction="row" alignItems="center" spacing={2}>
-                      <Avatar
-                        sx={{
-                          width: 36,
-                          height: 36,
-                        }}
-                      >
-                        S
-                      </Avatar>
-                      <Box sx={{ display: 'flex', flexDirection: 'column', width: 'fit-content' }}>
-                        <Typography sx={{ width: 'fit-content' }}>
-                          Sorawee Sripakdeepongdej
-                        </Typography>
-                        <Typography
-                          component="span"
-                          variant="caption"
-                          color="text.disabled"
-                          sx={{ mt: 0.5, width: 'fit-content' }}
-                        >
-                          642115045
-                        </Typography>
-                      </Box>
-                    </Stack>
-                  </TableCell>
-                  <TableCell align="left" sx={{ borderRadius: 0 }}>
-                    <Typography>sorawee_sri@cmu.ac.th</Typography>
-                  </TableCell>
-                  {role === 'ProfAcc' && (
-                    <TableCell
-                      align="right"
-                      sx={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
-                    >
-                      <IconButton onClick={popover.onOpen}>
-                        <Iconify icon="eva:more-vertical-fill" />
-                      </IconButton>
-                    </TableCell>
-                  )}
-                </TableRow>
-              </TableBody>
-            </Table>
-          </Scrollbar>
-        </TableContainer>
+        <MemberList users={classroom?.students} />
       </Stack>
     </Container>
   );
