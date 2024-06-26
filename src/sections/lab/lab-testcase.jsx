7d4276b8f9af89307b2f89af8d8e913e32f02a2b
webwebweb5 <phiriyakornman@gmail.com>
2024-06-26 14:44:42 +0700

    Mock check test case

--- src/sections/lab/lab-testcase.jsx
+++ src/sections/lab/lab-testcase.jsx
@@ -10,7 +10,7 @@ import Iconify from 'src/components/iconify';
 // Define tab labels for the main tabs
 const TABS = [
   { value: 'one', label: 'Test Case' },
-  { value: 'two', label: 'Terminal' },
+  { value: 'two', label: 'Test Case Result' },
 ];
 
 // Custom Tab styling
@@ -39,7 +39,7 @@ const CustomTabs = styled(Tabs)(({ theme }) => ({
     borderRadius: '6px',
     lineHeight: 0,
     minHeight: 'unset',
-    padding: '16px',
+    padding: '8px 12px',
     color: 'grey',
     fontWeight: 700,
     transition: 'all 0.3s ease',
@@ -94,7 +94,15 @@ export default function LabTestCase({
           <Stack sx={{ px: 2, pb: 1 }}>
             <CustomTabs value={currentTestCaseTab} onChange={handleChangeTestCaseTab}>
               {testCases.map((_, index) => (
-                <CustomTab key={index} value={index} label={`Case ${index + 1}`} />
+                <CustomTab
+                  key={index}
+                  value={index}
+                  label={
+                    <Stack direction="row" alignItems="center" spacing={1}>
+                      <Iconify icon="tabler:test-pipe" /> Case {index + 1}
+                    </Stack>
+                  }
+                />
               ))}
             </CustomTabs>
           </Stack>
@@ -117,36 +125,66 @@ export default function LabTestCase({
         </>
       ) : (
         currentTab === 'two' && (
-          <Stack>
-            <Stack sx={{ pt: 1 }} direction="row">
-              <Stack
-                sx={{ px: 1, backgroundColor: 'primary.main', width: 'fit-content' }}
-                direction="row"
-                alignItems="center"
-              >
-                <Iconify icon="quill:folder-open" sx={{ mr: 1 }} />
-                <Typography variant="body2">~/{user.userName}@questify</Typography>
-              </Stack>
-              <Box
-                sx={{
-                  width: 0,
-                  height: 0,
-                  borderTop: '12px solid transparent',
-                  borderBottom: '12px solid transparent',
-                  borderLeft: (theme) => `solid 12px ${theme.palette.primary.main}`,
-                }}
-              />
+          // <Stack sx={{ px: 2, pb: 2 }}>
+          //   <Typography variant="h4" color="success.main" sx={{ py: 2, pt: 1 }}>
+          //     Accepted
+          //   </Typography>
+          //   <Stack>
+          //     <CustomTabs value={currentTestCaseTab} onChange={handleChangeTestCaseTab}>
+          //       <CustomTab
+          //         value={0}
+          //         label={
+          //           <Stack direction="row" alignItems="center" spacing={1}>
+          //             <Iconify icon="line-md:check-all" sx={{ color: 'primary.main' }} /> Case 1
+          //           </Stack>
+          //         }
+          //       />
+          //     </CustomTabs>
+          //   </Stack>
+          //   <Stack>
+          //     <Box>
+          //       <Stack spacing={1}>
+          //         <Typography variant="subtitle2">Input =</Typography>
+          //         <Box sx={{ p: 2, borderRadius: 1, bgcolor: '#1B212A' }}>5</Box>
+          //         <Typography variant="subtitle2">Expected Output =</Typography>
+          //         <Box sx={{ p: 2, borderRadius: 1, bgcolor: '#1B212A' }}>120</Box>
+          //         <Typography variant="subtitle2">Output =</Typography>
+          //         <Box sx={{ p: 2, borderRadius: 1, bgcolor: '#1B212A', color: 'success.main' }}>
+          //           120
+          //         </Box>
+          //       </Stack>
+          //     </Box>
+          //   </Stack>
+          // </Stack>
+          <Stack sx={{ px: 2, pb: 2 }}>
+            <Typography variant="h4" color="error.main" sx={{ py: 2, pt: 1 }}>
+              Failed
+            </Typography>
+            <Stack>
+              <CustomTabs value={currentTestCaseTab} onChange={handleChangeTestCaseTab}>
+                <CustomTab
+                  value={0}
+                  label={
+                    <Stack direction="row" alignItems="center" spacing={1}>
+                      <Iconify icon="line-md:close-small" sx={{ color: 'error.main' }} /> Case 1
+                    </Stack>
+                  }
+                />
+              </CustomTabs>
             </Stack>
-            <Stack sx={{ px: 2, py: 1 }}>
-              {result ? (
-                result.map((line, i) => (
-                  <p key={i} className={`${isCompileError.value ? 'text-red-500' : ''}`}>
-                    &gt; {line}
-                  </p>
-                ))
-              ) : (
-                <p>&gt;</p>
-              )}
+            <Stack>
+              <Box>
+                <Stack spacing={1}>
+                  <Typography variant="subtitle2">Input =</Typography>
+                  <Box sx={{ p: 2, borderRadius: 1, bgcolor: '#1B212A' }}>5</Box>
+                  <Typography variant="subtitle2">Expected Output =</Typography>
+                  <Box sx={{ p: 2, borderRadius: 1, bgcolor: '#1B212A' }}>120</Box>
+                  <Typography variant="subtitle2">Output =</Typography>
+                  <Box sx={{ p: 2, borderRadius: 1, bgcolor: '#1B212A', color: 'error.main' }}>
+                    60
+                  </Box>
+                </Stack>
+              </Box>
             </Stack>
           </Stack>
         )
